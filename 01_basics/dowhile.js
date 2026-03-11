c8da8f48b64a72257c0486e070295e53e1e4f867
sdrathod1307 <suraj.rathod1307@gmail.com>
2026-03-11 17:36:23 +0000

    dowhile for

---
+++ 01_basics/dowhile.js
@@ -0,0 +1,7 @@
+//do while
+
+let score = 1
+do {
+    console.log(`score is ${score}`)
+    score ++;
+} while (score <= 10);
--- 01_basics/for.js
+++ 01_basics/for.js
@@ -14,7 +14,7 @@ for (let i = 0; i <= 10; i++) {
 for (let i = 0; i <= 10; i++) {
   console.log('outer loop value: {i}');
   for (let j = 0; j <= 10; j++) {
-    console.log('inner loop value: ${j} and inner loop ${i}');
+    console.log(`inner loop value: ${j} and inner loop ${i}`);
     
     
   }
